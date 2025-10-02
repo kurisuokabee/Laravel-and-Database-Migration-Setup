0f6babf4bdce1bcacbaf0040794c5fefa7657876
Angelo Tactay <vangelo0606@gmail.com>
2025-10-02 19:09:50 +0800

    Files

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {   
+        $firstName = $this->faker->firstName;
+        $lastName = $this->faker->lastName;
+        $contact = $this->faker->phoneNumber;
+
+        return [
+            'first_name' => substr($firstName, 0, 30),
+            'last_name' => substr($lastName, 0, 30),
+            'contact' => substr($contact, 0, 15),
+            'address' => fake()->address()
+            
+        ];
+    }
+}
